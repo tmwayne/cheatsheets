529e733d75489f331afb236c9778dbc47aef8507
Tyler <tylerwayne3@gmail.com>
2020-04-24 22:43:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Cheatsheets
+
+Print cheatsheets to screen to remind of useful functions
+
+## Install
+
+Clone this repo and create a symlink to a directory such as ~/.local/bin
+
+Name the symlink `cs` for easy typing on the command line.
+
+Set `CHEATSHEETS_DIR` environment variable to directory with cheatsheets.
+
+## Usage
+
+To view a cheatsheet simply type the name of the sheet.
+
+    cs linux
+
+will search the CHEATSHEETS_DIR for a file named linux.txt and print to screen.
+
+If the file is too long for the screen, it will be paged using `less`.
+
+## Cheatsheets
+
+Feel free to use my collection of cheatsheets located at
+`github.com/tmwayne/cheatsheet-files`
---
+++ cheatsheets.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# Cheatsheets
+#
+# Print a cheatsheet to the terminal to remind of useful commands
+#
+# Tyler Wayne © 2020
+
+CUR_DIR=$( dirname $0 )
+THIS_PROG=$( basename $0 )
+USAGE="Usage: ${THIS_PROG} cheatsheet"
+
+Help() {
+  # Function to display help at command line
+  echo $USAGE
+  echo "Print cheatsheet to terminal with paging."
+  echo
+  echo "Not many options at the moment:"
+  echo "  -h, --help                Print this help."
+  echo
+}
+
+## VARIABLES
+########################################
+
+# Command-line arguments
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    --help)         set -- "$@" "-h" ;;
+    --*)            echo "$THIS_PROG: unrecognized option '$arg'" >&2
+                    echo "Try '$THIS_PROG --help' for more information."
+                    exit 2 ;;
+    *)              set -- "$@" "$arg"
+  esac
+done
+
+OPTIND=1
+while getopts ":h" opt; do
+  case $opt in
+    h)  Help; exit 0 ;;
+    \?) echo "$THIS_PROG: unrecognized option '-$OPTARG'" >&2
+        echo "Try '$THIS_PROG --help' for more information."
+        exit 2 ;;
+  esac
+done
+shift $((OPTIND-1))
+
+CS_DIR=${CHEATSHEETS_DIR:-/home/tyler/scripts/cheatsheets/}
+
+CS=$CS_DIR/$1.txt
+
+## ASSERTIONS
+########################################
+
+if [ $# -lt 1 ]; then
+  echo $USAGE
+  exit 1
+fi
+
+
+## MAIN
+########################################
+
+if [ -f "$CS" ]; then
+
+  file_length=`wc -l $CS | cut -d' ' -f1`
+  term_length=`tput lines`
+
+  if [ $file_length -gt $term_length ]; then
+    output_pager=less
+  else
+    output_pager=cat
+  fi
+
+  cat $CS | $output_pager
+fi
+
