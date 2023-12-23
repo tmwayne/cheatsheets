6a641016a93eb38809a496956e501b9b48135e5d
Tyler Wayne <tylerwayne3@gmail.com>
2023-12-23 09:04:36 -0800

    update default directory to .cheatsheets.d for cs

--- cheatsheets.sh
+++ cheatsheets.sh
@@ -46,7 +46,7 @@ default="cheat"
 action=$default
 
 # Environment variables
-cs_dir=${CHEATSHEETS_DIR:-$HOME/docs/cheatsheets}
+cs_dir=${CHEATSHEETS_DIR:-$HOME/.cheatsheets.d}
 
 # Command-line arguments
 for arg in "$@"; do
